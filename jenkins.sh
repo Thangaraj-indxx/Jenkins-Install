1fecbfbe522b759587142db858daec871f2df899
Thangaraj-indxx <141306308+Thangaraj-indxx@users.noreply.github.com>
2024-04-13 11:34:29 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -12,16 +12,22 @@ apt install -y openjdk-8-jdk
 
 # Download and install Jenkins Debian package
 wget -q -O /tmp/jenkins.deb https://pkg.jenkins.io/debian-stable/binary/jenkins_2.337_all.deb
-dpkg -i /tmp/jenkins.deb
 
-# Start Jenkins service
-systemctl start jenkins
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+  dpkg -i /tmp/jenkins.deb
 
-# Enable Jenkins to start on boot
-systemctl enable jenkins
+  # Start Jenkins service
+  systemctl start jenkins
 
-# Print initial admin password location and instructions
-echo "Jenkins installation completed."
-echo "Please retrieve your initial admin password from the following location:"
-echo "/var/lib/jenkins/secrets/initialAdminPassword"
-echo "Then, access Jenkins using your server's IP or domain name on port 8080."
+  # Enable Jenkins to start on boot
+  systemctl enable jenkins
+
+  # Print initial admin password location and instructions
+  echo "Jenkins installation completed."
+  echo "Please retrieve your initial admin password from the following location:"
+  echo "/var/lib/jenkins/secrets/initialAdminPassword"
+  echo "Then, access Jenkins using your server's IP or domain name on port 8080."
+else
+  echo "Failed to download Jenkins Debian package. Please check your internet connection."
+fi
